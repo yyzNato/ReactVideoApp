44341e64d226039df0101318cd44910839c579b5
yyzNato <natalio.corrionero@lateralview.net>
2017-02-28 20:17:57 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React Video Application
+
+This app uses Youtube's API for search, list and watch videos.
---
+++ src/index.js
@@ -0,0 +1,44 @@
+import _ from 'lodash';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+import YTSearch from 'youtube-api-search';
+
+const API_KEY = 'AIzaSyCfzv2NCOEuP-N_jH3cbDmIZQ34lcN3R0o';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('guitars');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render(){
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList onVideoSelect={selectedVideo => this.setState({selectedVideo})} videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
